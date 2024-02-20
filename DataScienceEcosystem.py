20723f9c7b876403db4f7054fc83aaef87e3460d
truong010 <124592870+truong010@users.noreply.github.com>
2024-02-20 23:53:56 +0700

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - Understand basic arithmetic operations in Python
+# - Convert units of measurement in Python
+# 
+
+# # Some of the popular languages that Data Scientists use are:
+# <ul>
+# <li>R</li>
+# <li>Python</li>
+# <li>Sql</li>
+# </ul>
+
+# # Some of the commonly used libraries used by Data Scientists include:
+# <ul>
+# <li>Numpy</li>
+# <li>Pandas</li>
+# <li>Keras</li>
+# </ul>
+# 
+
+# #### Data Science Tools
+# 
+# | Công cụ          | Mô tả                                                               |
+# |------------------|----------------------------------------------------------------------|
+# | Jupyter Notebook | Một môi trường tính toán tương tác và trực quan cho ngôn ngữ Python |
+# | Pandas           | Thư viện mạnh mẽ cho phân tích và xử lý dữ liệu                      |
+# | Matplotlib       | Thư viện vẽ biểu đồ chung cho Python                                 |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+# 1. **Addition**: 
+# ```python
+# # Addition example
+# result = 10 + 5
+# print(result)  # Output: 15
+# 
+
+# In[4]:
+
+
+# This is a simple arithmetic expression to multiply then add integers.
+result = (3*4) + 5
+print(result)
+
+
+# In[6]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+result = 200 / 60
+print(result)
+
+
+# ## Author
+# 
+# My name is Truong.
+# 
+
+# In[ ]:
+
+
+
+
